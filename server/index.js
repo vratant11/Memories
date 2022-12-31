3df81e3f7f0b939083660fdb7186802098f73b6e
vratant11 <vratant2012061@akgec.ac.in>
2022-12-31 20:25:25 +0530

    Folders created

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://Memorise:rockandroll11@@cluster0.shxkrp9.mongodb.net/<dbname>?retryWrites=true&w=majority';
+  mongoose.set("strictQuery", true);
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+
