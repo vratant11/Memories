75460377c485e2aa2e6f0a3907a4c7ac5c598db4
vratant11 <vratant2012061@akgec.ac.in>
2023-01-07 22:03:48 +0530

    error

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+    app.listen(PORT, () => console.log(`Current port : ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
